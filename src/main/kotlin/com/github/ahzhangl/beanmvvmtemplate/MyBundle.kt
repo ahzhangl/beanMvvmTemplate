7dcbea2d6f9ef453a3b742ae13247cc7ca1b3da5
GitHub Action <action@github.com>
2022-03-15 05:41:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/ahzhangl/beanmvvmtemplate/MyBundle.kt
+++ src/main/kotlin/com/github/ahzhangl/beanmvvmtemplate/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.ahzhangl.beanmvvmtemplate
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/ahzhangl/beanmvvmtemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/ahzhangl/beanmvvmtemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.ahzhangl.beanmvvmtemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.ahzhangl.beanmvvmtemplate.MyBundle
 
 class MyApplicationService {
 
